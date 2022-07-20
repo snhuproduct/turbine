77e2db092b8180cefbd868548c1bb0026700d7e9
joao-sada <joao.furtado@sada.com>
2022-07-20 12:12:25 -0700

    SNHU-1414: removed user-test component

--- apps/toboggan-app/src/app/user/user.module.ts
+++ apps/toboggan-app/src/app/user/user.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StoriesModule } from '@snhuproduct/toboggan-ui-components-library';
-import { UserTestComponent } from './components/user-test/user-test.component';
 import { UserMainPageComponent } from './pages/user-main-page/user-main-page.component';
 
 @NgModule({
-  declarations: [UserTestComponent, UserMainPageComponent],
+  declarations: [UserMainPageComponent],
   imports: [CommonModule, StoriesModule],
-  exports: [UserTestComponent, UserMainPageComponent],
+  exports: [UserMainPageComponent],
 })
 export class UserModule {}
