2f41d13be5c0b8c14f285f7869062a5d865d2a11
joao-sada <joao.furtado@sada.com>
2022-07-20 11:46:04 -0700

    SNHU-1414: Storybook integration 90% done. Missing assets path

--- apps/toboggan-app/src/app/app.module.ts
+++ apps/toboggan-app/src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoriesModule } from '@snhuproduct/toboggan-ui-components-library';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, UserModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    StoriesModule,
+    HttpClientModule,
+    AppRoutingModule,
+    UserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/toboggan-app/src/app/user/user.module.ts
+++ apps/toboggan-app/src/app/user/user.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { StoriesModule } from '@snhuproduct/toboggan-ui-components-library';
 import { UserTestComponent } from './components/user-test/user-test.component';
 import { UserMainPageComponent } from './pages/user-main-page/user-main-page.component';
 
 @NgModule({
   declarations: [UserTestComponent, UserMainPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, StoriesModule],
   exports: [UserTestComponent, UserMainPageComponent],
 })
 export class UserModule {}
