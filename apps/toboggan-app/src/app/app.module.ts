3f6acc8127bc9cb696ff66f2485ad58e079884d2
dmitriy_rodimin_sada <dmitriy.rodimin@sada.com>
2022-07-20 12:53:11 -0400

    scaffolding

--- apps/toboggan-app/src/app/app.module.ts
+++ apps/toboggan-app/src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { StoriesModule } from "@snhuproduct/toboggan-ui-components-library";
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,BrowserModule, HttpClientModule, StoriesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
