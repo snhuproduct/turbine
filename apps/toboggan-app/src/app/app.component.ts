dbf3601db8bdbd835d1993eb8ed055b50bf737ad
joao-sada <joao.furtado@sada.com>
2022-07-22 12:26:30 -0700

    SNHU-1430: base support for SCSS

--- apps/toboggan-app/src/app/app.component.ts
+++ apps/toboggan-app/src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'toboggan-ws-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
--- apps/toboggan-app/src/app/user/pages/user-main-page/user-main-page.component.ts
+++ apps/toboggan-app/src/app/user/pages/user-main-page/user-main-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'toboggan-ws-user-main-page',
   templateUrl: './user-main-page.component.html',
-  styleUrls: ['./user-main-page.component.css'],
+  styleUrls: ['./user-main-page.component.scss'],
 })
 export class UserMainPageComponent {
   constructor() {}
