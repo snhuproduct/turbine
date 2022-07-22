baecb68d14035843848a7ee2c8c431f9d965d873
joao-sada <joao.furtado@sada.com>
2022-07-22 12:54:12 -0700

    SNHU-582: Finished banner component and service

--- apps/toboggan-app/src/app/user/pages/user-main-page/user-main-page.component.ts
+++ apps/toboggan-app/src/app/user/pages/user-main-page/user-main-page.component.ts
@@ -1,25 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { BannerService } from '../../../shared/services/banner/banner.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'toboggan-ws-user-main-page',
   templateUrl: './user-main-page.component.html',
   styleUrls: ['./user-main-page.component.scss'],
 })
-export class UserMainPageComponent implements OnInit {
-  constructor(private bannerService: BannerService) {}
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.bannerService.showBanner({
-        type: 'success',
-        message: 'This is a success banner',
-        heading: 'Success',
-        button: {
-          label: 'Dismiss',
-          action: () => this.bannerService.hideBanner(),
-        },
-      });
-    }, 2000);
-  }
-}
+export class UserMainPageComponent {}
