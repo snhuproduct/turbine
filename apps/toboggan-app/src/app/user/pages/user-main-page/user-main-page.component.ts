09534d1bb2d1e0cb97640ebe544b637133b827d4
joao-sada <joao.furtado@sada.com>
2022-07-26 15:45:24 -0700

    SNHUT-1437: husky pre commit and pre push hooks

--- apps/toboggan-app/src/app/user/pages/user-main-page/user-main-page.component.ts
+++ apps/toboggan-app/src/app/user/pages/user-main-page/user-main-page.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './user-main-page.component.html',
   styleUrls: ['./user-main-page.component.scss'],
 })
-export class UserMainPageComponent {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class UserMainPageComponent {}
