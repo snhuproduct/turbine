5b89f104af7d5d29f3d99d3fd2ffaa2912ac4760
Ajith P <ajith.padmakumar@sada.com>
2022-08-01 20:43:52 +0530

    SNHUT-1244-updated add user component

--- apps/toboggan-app/src/app/group/components/add-users/add-users.component.ts
+++ apps/toboggan-app/src/app/group/components/add-users/add-users.component.ts
@@ -10,67 +10,76 @@ import { GroupService } from '../../services/group.service';
   styleUrls: ['./add-users.component.scss'],
 })
 export class AddUsersComponent implements OnInit {
-  addUserForm: FormGroup = new FormGroup( {
+  addUserForm: FormGroup = new FormGroup({
     groupId: new FormControl(''),
-    user: new FormControl('', [Validators.required, Validators.email])
+    user: new FormControl('', [Validators.required, Validators.email]),
   });
   users: IUser[] = [];
   userEmails: string[] = [];
 
-  constructor(private userService: UserService, public groupService: GroupService){}
+  constructor(
+    private userService: UserService,
+    private groupService: GroupService
+  ) {}
 
   ngOnInit(): void {
-      this.getUsers();
+    this.getUsers();
   }
 
-  getUsers() :void {
-    this.userService.fetchUsers().subscribe((users:IUser[]) => {
+  getUsers(): void {
+    this.userService.fetchUsers().subscribe((users: IUser[]) => {
       this.users = users;
-      this.userEmails = this.users.map(user => user.email);
+      this.userEmails = this.users.map((user) => user.email);
     });
   }
-  
+
   addUsertoGroup() {
     const userEmail = this.addUserForm.value.user;
-    if(!this.userEmails.includes(userEmail)) {
-      this.addUserForm.get('user')?.setErrors({'incorrect': true});
+    if (!this.userEmails.includes(userEmail)) {
+      this.addUserForm.get('user')?.setErrors({ incorrect: true });
       return;
-    } 
-    if(this.addUserForm.valid ) {
-      this.groupService.addUsertoGroup(this.addUserForm.value.groupId, this.addUserForm.value.user).subscribe( {
-        next : (response) =>  { 
-          // handle success
-        },
-        error: (error) => { // handle error scenario
-        }
-        
-      });
     }
-    
+    if (this.addUserForm.valid) {
+      this.groupService
+        .addUsertoGroup(
+          this.addUserForm.value.groupId,
+          this.addUserForm.value.user
+        )
+        .subscribe({
+          next: (response) => {
+            // handle success
+          },
+          error: (error) => {
+            // handle error scenario
+          },
+        });
+    }
+
     return false;
   }
-  
+
   hasError(controlName: string) {
     const control = this.addUserForm.get(controlName);
     if (control) {
-      return (!control.valid ||  !this.userEmails.includes(control?.value)) && (control.dirty || control.touched)  ;
+      return (
+        (!control.valid || !this.userEmails.includes(control?.value)) &&
+        (control.dirty || control.touched)
+      );
     }
     return false;
   }
-  
+
   getFormError(field: string): string {
     const control = this.addUserForm.get(field);
-    if(control?.hasError('required')) {
-      return "This field can't be empty"
-    } 
-    if(control?.hasError('email')) {
-      return "Check email format";
-    } 
-    if(!this.userEmails.includes(control?.value)) {
-      return "This email doesn't exist"
-    } 
+    if (control?.hasError('required')) {
+      return "This field can't be empty";
+    }
+    if (control?.hasError('email')) {
+      return 'Check email format';
+    }
+    if (!this.userEmails.includes(control?.value)) {
+      return "This email doesn't exist";
+    }
     return '';
-     
   }
-  
 }
