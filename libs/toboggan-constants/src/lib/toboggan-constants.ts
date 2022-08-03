a6e528535c4ebffefc6d6038f0338266276a0229
Shafi Shamsudheen <shafi.shamsudheen@sada.com>
2022-08-03 21:12:50 +0530

    Changed to enum

--- libs/toboggan-constants/src/lib/toboggan-constants.ts
+++ libs/toboggan-constants/src/lib/toboggan-constants.ts
@@ -2,10 +2,9 @@ export function tobogganConstants(): string {
   return 'toboggan-constants';
 }
 
-
-export class FormError {
-  public static isRequired = ' is required';
-  public static empty = 'This field can’t be empty';
-  public static lettersAndNumbers = 'Use only letters and numbers';
-  public static characters = 'Don’t use these characters: ';
+export enum FormError {
+  isRequired = ' is required',
+  empty = 'This field can’t be empty',
+  lettersAndNumbers = 'Use only letters and numbers',
+  characters = 'Don’t use these characters: ',
 }
