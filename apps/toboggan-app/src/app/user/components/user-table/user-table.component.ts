35cbbcfb2e362919d687101f0d8982d05f21e180
joao-sada <joao.furtado@sada.com>
2022-08-03 10:29:20 -0700

    SNHUT-1233: user table component creation

---
+++ apps/toboggan-app/src/app/user/components/user-table/user-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'toboggan-ws-user-table',
+  templateUrl: './user-table.component.html',
+  styleUrls: ['./user-table.component.scss'],
+})
+export class UserTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- apps/toboggan-app/src/app/user/user.module.ts
+++ apps/toboggan-app/src/app/user/user.module.ts
@@ -6,9 +6,14 @@ import { SharedModule } from '../shared/shared.module';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { UserMainPageComponent } from './pages/user-main-page/user-main-page.component';
 import { userRouting } from './user.routing';
+import { UserTableComponent } from './components/user-table/user-table.component';
 
 @NgModule({
-  declarations: [UserMainPageComponent, CreateUserComponent],
+  declarations: [
+    UserMainPageComponent,
+    CreateUserComponent,
+    UserTableComponent,
+  ],
   imports: [
     CommonModule,
     StoriesModule,
