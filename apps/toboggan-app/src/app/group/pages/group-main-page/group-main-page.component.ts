e3e0ece1f24e2689d3080006b2c55fdc0891bca5
Shafi Shamsudheen <shafi.shamsudheen@sada.com>
2022-08-16 04:40:52 +0530

    Comments added

--- apps/toboggan-app/src/app/group/pages/group-main-page/group-main-page.component.ts
+++ apps/toboggan-app/src/app/group/pages/group-main-page/group-main-page.component.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import {
   ModalButtonConfig,
-  ModalComponent,
+  ModalComponent
 } from '@snhuproduct/toboggan-ui-components-library';
 import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { AddUsersComponent } from '../../components/add-users/add-users.component';
@@ -40,7 +42,7 @@ export class GroupMainPageComponent {
   addUserModalRef?: BsModalRef | null;
   addUserModalState!: ModalOptions;
 
-  constructor(private modalService: BsModalService) {}
+  constructor(private modalService: BsModalService) { }
 
   openAddUserModal() {
     this.modalService._hideModal();
