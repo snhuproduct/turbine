8c6c8e2d54dac8fe348e1815941d363e48c2e771
Ajith P <ajith.padmakumar@sada.com>
2022-08-24 14:51:51 +0530

    Added safe html pipe to support html messages

--- apps/toboggan-app/src/app/shared/shared.module.ts
+++ apps/toboggan-app/src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { StoriesModule } from '@snhuproduct/toboggan-ui-components-library';
 import { BannerComponent } from './components/banner/banner.component';
 import { ModalAlertComponent } from './components/modal-alert/modal-alert/modal-alert.component';
+import { SafeHtmlPipe } from './pipes/safe-html.pipe';
 
 @NgModule({
-  declarations: [BannerComponent, ModalAlertComponent],
+  declarations: [BannerComponent, ModalAlertComponent, SafeHtmlPipe],
   imports: [CommonModule, StoriesModule],
   exports: [CommonModule, BannerComponent, ModalAlertComponent, StoriesModule],
 })
