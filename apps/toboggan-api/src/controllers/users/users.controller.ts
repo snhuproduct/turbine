470393f0135523aa39e61edda0e946b71c9b04a6
Mehran Khan <mehran.khan@sada.com>
2022-08-25 18:15:12 +0000

    password-reset request scaffolding at the back-end

--- apps/toboggan-api/src/controllers/users/users.controller.ts
+++ apps/toboggan-api/src/controllers/users/users.controller.ts
@@ -3,11 +3,13 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Patch,
   Post,
   Put,
-  Query,
+  Query
 } from '@nestjs/common';
 import { IUser } from '@toboggan-ws/toboggan-common';
 import { UsersService } from '../../providers/users/users.service';
@@ -37,10 +39,24 @@ export class UsersController {
     return this.usersService.updateUser(id, user);
   }
 
+  @Put('/:id/password')
+  resetPasswordOfUser(@Param('id') id, @Body() operationBody: {
+    type: string
+  } ) {
+    switch(operationBody.type){
+      case ('reset'):
+        this.usersService.resetPasswordOfUser(id);
+        break;
+      default:
+        throw new HttpException({status: HttpStatus.BAD_REQUEST, error:`Invalid request-body!`}, HttpStatus.BAD_REQUEST);
+    }
+  }
+
   @Patch('/:id')
   patchUser(@Param('id') id, @Body() user: IUser) {
     return this.usersService.patchUser(id, user);
   }
+
 
   @Delete('/:id')
   deleteUser(@Param('id') id) {
