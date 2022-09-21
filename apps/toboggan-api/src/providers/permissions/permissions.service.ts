7ce502c2801a8a431dd9d0b6eae7f7eb52470623
Ajith P <ajith.padmakumar@sada.com>
2022-09-21 11:59:06 +0530

    Added api unit test cases

--- apps/toboggan-api/src/providers/permissions/permissions.service.ts
+++ apps/toboggan-api/src/providers/permissions/permissions.service.ts
@@ -5,65 +5,25 @@ import {
   IPermission,
 } from '@toboggan-ws/toboggan-common';
 import * as arrayPaginate from 'array-paginate';
+import { mockPermissions } from './permissions.mock';
 
 @Injectable()
 export class PermissionService {
   private permissions: IPermission[] = [];
   private groups: IGroup[] = [];
-  private accessLevels = [
-    'View',
-    'Create',
-    'Edit',
-    'Reset Password',
-    'Activate/Deactivate',
-    'Archive',
-    'Change Color',
-    'Publish',
-  ];
-  private modules = [
-    'User',
-    'User Groups',
-    'Permissions',
-    'Learning Resource',
-    'Content Object',
-    'Learning Experience',
-    'Assessment Item',
-  ];
 
   constructor() {
-    const groupList = [
-      'Content Management',
-      'Content Authoring',
-      'Assessment Authoring',
-    ];
-    // generate mocked data for 20 users
-    for (let i = 0, j = 0, k = 0, m = 0; i < 20; i++) {
-      if (j == this.accessLevels.length) j = 0;
-      if (k == this.modules.length) k = 0;
-      if (m == groupList.length) m = 0;
-      const group = {
-        id: `group-id-${i}`,
-        name: groupList[m],
-        description: '',
-      };
-      this.permissions.push({
-        id: `id-${i}`,
-        application: 'Application Admin',
-        module: this.modules[k],
-        accessLevel: this.accessLevels[j],
-        userGroups: [group],
-      });
-      j++;
-      k++;
-      m++;
-    }
+    this.permissions = mockPermissions;
   }
 
   getPermissions() {
     return this.permissions;
   }
 
-  getPaginatedPermissions(currentPage: number, resultsPerPage = 10): IGroup[] {
+  getPaginatedPermissions(
+    currentPage: number,
+    resultsPerPage = 10
+  ): IPermission[] {
     const paginatedPermissions = arrayPaginate(
       this.groups,
       currentPage,
