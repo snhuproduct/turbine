ed2d3e7726826efb654e57941911318be2e8f8da
Ajith P <ajith.padmakumar@sada.com>
2022-09-21 11:20:27 +0530

    updated unit tests

--- apps/toboggan-app/src/app/permission/pages/permissions-main-page/permissions-main-page.component.spec.ts
+++ apps/toboggan-app/src/app/permission/pages/permissions-main-page/permissions-main-page.component.spec.ts
@@ -1,4 +1,10 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoriesModule } from '@snhuproduct/toboggan-ui-components-library';
+import { SharedModule } from '../../../shared/shared.module';
+import { PermissionsListComponent } from '../../components/permissions-list/permissions-list.component';
 
 import { PermissionsMainPageComponent } from './permissions-main-page.component';
 
@@ -8,7 +14,14 @@ describe('PermissionsMainPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [PermissionsMainPageComponent],
+      imports: [
+        StoriesModule,
+        NoopAnimationsModule,
+        SharedModule,
+        RouterTestingModule,
+      ],
+      declarations: [PermissionsMainPageComponent, PermissionsListComponent],
+      providers: [HttpClient, HttpHandler],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PermissionsMainPageComponent);
