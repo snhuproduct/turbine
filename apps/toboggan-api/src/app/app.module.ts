7d7e7aead201261182a8e209c343694327577c4c
Joao <107008166+joao-sd@users.noreply.github.com>
2022-10-31 10:27:27 -0700

    Revert "SNHUT-2308: List of assessments"

--- apps/toboggan-api/src/app/app.module.ts
+++ apps/toboggan-api/src/app/app.module.ts
@@ -5,17 +5,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { GroupsModule } from './modules/groups/groups.module';
 import { PermissionsModule } from './modules/permissions/permissions.module';
 import { UsersModule } from './modules/users/users.module';
-import { AssessmentsModule } from './modules/assessments/assessments.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    UsersModule,
-    GroupsModule,
-    PermissionsModule,
-    AssessmentsModule,
-  ],
+  imports: [AuthModule, UsersModule, GroupsModule, PermissionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
