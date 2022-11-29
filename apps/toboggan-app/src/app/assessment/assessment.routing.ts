c566ef00330569055ce9369b41a1168d27e3a431
lekshmic-sadaindia <118151666+lekshmic-sadaindia@users.noreply.github.com>
2022-11-29 10:56:58 +0530

    Snhut 2640 assessment details page (#113)
    * SNHUT:2640 - Open Submission Details Page from List View - container
    
    * SNHUT:2640 - eslint error fixed
    
    * SNHUT-2640: Addressed review comments
    
    * SNHUT-2640: review comments updated

--- apps/toboggan-app/src/app/assessment/assessment.routing.ts
+++ apps/toboggan-app/src/app/assessment/assessment.routing.ts
@@ -1,8 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
+import { AssessmentDetailsComponent } from './components/assessment-details/assessment-details.component';
 import { AssessmentMainPageComponent } from './pages/assessment-main-page/assessment-main-page.component';
 
 const assessmentRoutes: Routes = [
   { path: '', component: AssessmentMainPageComponent },
+  { path: 'details/:id', component: AssessmentDetailsComponent }
 ];
 
 export const assessmentRouting = RouterModule.forChild(assessmentRoutes);
--- apps/toboggan-app/src/app/assessment/components/assessment-list/assessment-list.component.spec.ts
+++ apps/toboggan-app/src/app/assessment/components/assessment-list/assessment-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { StoriesModule } from '@snhuproduct/toboggan-ui-components-library';
 import { of } from 'rxjs';
 import { AssessmentService } from '../../services/assessment.service';
@@ -19,6 +20,10 @@ describe('AssessmentListComponent with empty results ', () => {
       declarations: [AssessmentListComponent],
       providers: [
         { provide: AssessmentService, useValue: mockAssessmentService },
+        {
+          provide: Router,
+          useValue: {},
+        }
       ],
     }).compileComponents();
 
