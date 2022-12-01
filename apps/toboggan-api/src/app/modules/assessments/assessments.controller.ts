973a3a5d4c6dcb31a8eb6e8929fe3873b17d51e4
artem-sada <110631003+artem-sada@users.noreply.github.com>
2022-12-01 15:07:55 +0300

    Assessment Management | List of evaluated assessments (MVP) - Frontend (#110)
    * SNHUT-1985: add cell types
    
    * SNHUT-2008: create assessment-evaluated-list
    
    * SNHUT-2008: move mocks to backend
    
    * SNHUT-2008: add flagged parameter
    
    * SNHUT-2008: add onLogOutClick attribute to gp-page on user main page
    
    * SNHUT-2008: add forest background to assessments page
    
    * Review changes
    
    Co-authored-by: Shafi Shamsudheen <shafi.shamsudheen@sada.com>
    Co-authored-by: Ajith P <109214920+sada-ajith@users.noreply.github.com>
    Co-authored-by: Shafi Shamsudheen <102204577+shafi-sadaindia@users.noreply.github.com>

--- apps/toboggan-api/src/app/modules/assessments/assessments.controller.ts
+++ apps/toboggan-api/src/app/modules/assessments/assessments.controller.ts
@@ -1,10 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Put,
   UseGuards,
-  UseInterceptors
+  UseInterceptors,
 } from '@nestjs/common';
 import { HTTPHeaderAuthGuard } from '../auth/http-header-auth-guard.service';
 import { TokenInterceptor } from '../auth/token.interceptor';
@@ -17,9 +18,19 @@ import { IAssessmentFlag } from './assessments.types';
 @UseInterceptors(TokenInterceptor, ResponseInterceptor, RequestInterceptor)
 @Controller('assessments')
 export class AssessmentsController {
-  constructor(private readonly assessmentsService: AssessmentsService) { }
+  constructor(private readonly assessmentsService: AssessmentsService) {}
 
-//Update Flag assessment
+  @Get('/')
+  getAssessments() {
+    return this.assessmentsService.getAssessments();
+  }
+
+  @Get('/evaluated')
+  getEvaluatedAssessments() {
+    return this.assessmentsService.getEvaluatedAssessments();
+  }
+
+  //Update Flag assessment
   @Put('/:uuid')
   updateFlagStatus(@Param('uuid') uuid, @Body() body: IAssessmentFlag) {
     return this.assessmentsService.updateFlagStatus(uuid, body);
